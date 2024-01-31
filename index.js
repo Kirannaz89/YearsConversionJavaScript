858fec180452e504096ad1011cef2cf63d26cf48
Kirannaz89 <Nazkiran1989.gmail.com>
2024-01-31 20:44:32 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+
+function howManyDays() {
+
+	var years = document.getElementById("years").value;
+	var result = years * 365;
+	document.getElementById("days").value = result;
+
+
+	
+}
+
+
+function howManyHours() {
+
+	var years = document.getElementById("Years").value;
+	var result = years * 8766;
+	document.getElementById("hours").value = result;
+
+
+	
+}
